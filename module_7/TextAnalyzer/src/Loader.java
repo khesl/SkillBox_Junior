ae9e81e79763cc9b184c454751d639a185005831
vassina <vassin.a@asiacreditbank.kz>
2018-11-14 19:58:20 +0600

    start 7 module

---
+++ module_7/TextAnalyzer/src/Loader.java
@@ -0,0 +1,18 @@
+package module_7.TextAnalyzer.src;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class Loader
+{
+    public static void main(String[] args) throws Exception
+    {
+        //Reading file to the String
+        String text = new String(Files.readAllBytes(Paths.get("res/text_01.txt")));
+
+        TextAnalyzer analyzer = new TextAnalyzer(text);
+        System.out.println("Most frequent word: " + analyzer.getMostFrequentWord());
+    }
+
+
+}
---
+++ module_7/src/Cities.java
@@ -0,0 +1,61 @@
+package module_7.src;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Cities
+{
+    public static String cities[] = {
+            "Abakan", "Belgorod", "Chelyabinsk", "Dzerzhinsk", "Ekaterinburg",
+            "Gelendzhik", "Irkutsk", "Krasnoyarsk", "Lipetsk", "Moscow",
+            "Novgorod", "Omsk", "Petrozavodsk", "Ryazan", "Samara", "Tomsk",
+            "Ufa", "Volgograd", "Yakutsk", "Zvenigorod"
+    };
+
+    public static void main(String[] args) throws IOException
+    {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String previousCity = "";
+        System.out.println("K".toLowerCase().charAt(0) == 'k');
+        for(;;)
+        {
+            try {
+                System.out.println(previousCity.length() == 0 ?
+                        "Please, type first city:" : "Please, type next city name:");
+
+                String city = reader.readLine();
+                if (previousCity.length() > 0 && !isNextCity(previousCity, city)) {
+                    System.out.println("This city is wrong! Try again!");
+                    continue;
+                }
+                String nextCity = searchNextCity(city);
+                System.out.println("My city is: " + nextCity);
+                previousCity = nextCity;
+            }
+            catch (Exception e){
+                // была ошибка при нулевом вводе, когда city.length() == 0
+                continue;
+            }
+        }
+    }
+
+    //=========================================================================
+
+    private static String searchNextCity(String currentCity)
+    {
+        String foundCity = null;
+        for(String city : cities) {
+            if(isNextCity(currentCity, city)) {
+                foundCity = city;
+            }
+        }
+        return foundCity;
+    }
+
+    private static boolean isNextCity(String currentCity, String nextCity) {
+        int currentCityLastChar = currentCity.length() - 1;
+        return currentCity.charAt(currentCityLastChar) ==
+            nextCity.toLowerCase().charAt(0);
+    }
+}
